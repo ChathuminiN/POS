496b40e1d41271e5d362edd07c978968db21b521
ChathuminiN <nuwanthika.whc93@gmail.com>
2024-09-29 01:30:15 +0530

    create spring boot and mysql connectivity

---
+++ src/main/java/com/ijse/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.ijse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello SpringBoot";
+    }
+
+    @PostMapping("/hello")
+    public String writeHello(){
+        return "Hello SpringBoot Write";
+    }
+
+
+}
